58a64788433ae8acc8befe9e6c88b251b9b7784d
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-10 18:23:36 +0530

    Converted the match code into different funtions.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use clap::{Parser, Subcommand};
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
+
 #[derive(Subcommand)]
 enum Commands {
     Python3 { fname: String },
@@ -12,28 +14,40 @@ enum Commands {
     Shell { fname: String },
     Bash { fname: String },
 }
+
+struct File {
+    filename: String,
+}
+
+impl File {
+    fn file_python3(fname: String) -> File {
+        let filename = format!("{}.py", fname);
+        println!("{} - Python 3", filename);
+        File { filename }
+    }
+    fn file_python2(fname: String) -> File {
+        let filename = format!("{}.py", fname);
+        println!("{} - Python 2", filename);
+        File { filename }
+    }
+    fn file_shell(fname: String) -> File {
+        let filename = format!("{}.sh", fname);
+        println!("{} - Shell", filename);
+        File { filename }
+    }
+    fn file_bash(fname: String) -> File {
+        let filename = format!("{}.bash", fname);
+        println!("{} - Bash", filename);
+        File { filename }
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
-    let _filename: String = match &cli.command {
-        Commands::Python3 { fname } => {
-            let filename = format!("{}.py", fname);
-            println!("{} - Python 3", filename);
-            filename
-        }
-        Commands::Python2 { fname } => {
-            let filename = format!("{}.py", fname);
-            println!("{} - Python 2", filename);
-            filename
-        }
-        Commands::Shell { fname } => {
-            let filename = format!("{}.sh", fname);
-            println!("{} - Shell", filename);
-            filename
-        }
-        Commands::Bash { fname } => {
-            let filename = format!("{}.bash", fname);
-            println!("{} - Bash", filename);
-            filename
-        }
+    let filename: File = match cli.command {
+        Commands::Python3 { fname } => File::file_python3(fname),
+        Commands::Python2 { fname } => File::file_python2(fname),
+        Commands::Shell { fname } => File::file_shell(fname),
+        Commands::Bash { fname } => File::file_bash(fname),
     };
 }
