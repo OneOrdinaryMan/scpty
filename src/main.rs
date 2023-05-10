b9cb263ad9f727940cbb073b4e1e30f31ab7aa29
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-10 21:30:43 +0530

    Created the file struct and abstracted code away.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::{fs, io::Result, path::Path, process};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -15,36 +16,96 @@ enum Commands {
     Bash { fname: String },
 }
 
+enum Filetype {
+    Python3,
+    Python2,
+    Shell,
+    Bash,
+}
+
 struct File {
     filename: String,
+    filetype: Filetype,
 }
 
 impl File {
-    fn file_python3(fname: String) -> File {
+    fn file_python3(fname: String) -> Result<File> {
         let filename = format!("{}.py", fname);
         println!("{} - Python 3", filename);
-        File { filename }
+        match Path::new(&filename).try_exists() {
+            Ok(value) => {
+                if !value {
+                    Ok(File {
+                        filename,
+                        filetype: Filetype::Python3,
+                    })
+                } else {
+                    println!("The file exists. Terminating!");
+                    process::exit(1);
+                }
+            }
+            Err(e) => Err(e),
+        }
     }
-    fn file_python2(fname: String) -> File {
+    fn file_python2(fname: String) -> Result<File> {
         let filename = format!("{}.py", fname);
         println!("{} - Python 2", filename);
-        File { filename }
+        match Path::new(&filename).try_exists() {
+            Ok(value) => {
+                if !value {
+                    Ok(File {
+                        filename,
+                        filetype: Filetype::Python2,
+                    })
+                } else {
+                    println!("The file exists. Terminating!");
+                    process::exit(1);
+                }
+            }
+            Err(e) => Err(e),
+        }
     }
-    fn file_shell(fname: String) -> File {
+    fn file_shell(fname: String) -> Result<File> {
         let filename = format!("{}.sh", fname);
         println!("{} - Shell", filename);
-        File { filename }
+        match Path::new(&filename).try_exists() {
+            Ok(value) => {
+                if !value {
+                    Ok(File {
+                        filename,
+                        filetype: Filetype::Shell,
+                    })
+                } else {
+                    println!("The file exists. Terminating!");
+                    process::exit(1);
+                }
+            }
+            Err(e) => Err(e),
+        }
     }
-    fn file_bash(fname: String) -> File {
+    fn file_bash(fname: String) -> Result<File> {
         let filename = format!("{}.bash", fname);
         println!("{} - Bash", filename);
-        File { filename }
+        match Path::new(&filename).try_exists() {
+            Ok(value) => {
+                if !value {
+                    Ok(File {
+                        filename,
+                        filetype: Filetype::Bash,
+                    })
+                } else {
+                    println!("The file exists. Terminating!");
+                    process::exit(1);
+                }
+            }
+            Err(e) => Err(e),
+        }
     }
 }
 
 fn main() {
     let cli = Cli::parse();
-    let filename: File = match cli.command {
+    let file = match cli.command {
         Commands::Python3 { fname } => File::file_python3(fname),
         Commands::Python2 { fname } => File::file_python2(fname),
         Commands::Shell { fname } => File::file_shell(fname),
